362d46848de04b1447b6c4044f3217e955e3501d
dgkeyes <dgkeyes@gmail.com>
2018-06-11 11:43:57 -0700

    June 11

--- functions/dk_functions.R
+++ functions/dk_functions.R
@@ -1,5 +1,13 @@
+library(captioner)
 library(tidyverse)
+library(readxl)
+library(googlesheets)
+library(janitor)
+library(skimr)
+library(lubridate)
+library(flextable)
 library(scales)
+library(lettercase)
 
 
 # Summarize categorical ---------------------------------------------------
@@ -84,6 +92,76 @@ dk_delete_mna_extras <- function(item) {
 }
 
 
+
+
+
+# Word table --------------------------------------------------------------
+
+
+dk_word_table <- function(df) {
+     df %>%
+          set_names(c('Response', 'Number')) %>%
+          regulartable %>%
+          width(width = 3) %>%
+          theme_zebra() %>%
+          align(align = "left", part = 'all') %>%
+          fontsize(size = 11) %>%
+          padding(padding = 10, part = 'all')
+}
+
+
+# Simple bar charts -------------------------------------------------------
+
+
+dk_bar_chart_n <- function(df, lower_limit, upper_limit) {
+     df %>%
+          set_names(c("item", "n")) %>%
+          ggplot(aes(x = reorder(item, n), 
+                     y = n)) +
+          geom_col(fill = mna.blue) +
+          geom_text(aes(label = n),
+                    color = 'white',
+                    hjust = 1.65) +
+          scale_y_continuous(limits = c(lower_limit, upper_limit),
+                             breaks = pretty_breaks()) +
+          coord_flip() +
+          mna.bar.chart.theme
+     
+}
+
+
+dk_bar_chart_pct <- function(df) {
+     df %>%
+          ggplot(aes(x = item, 
+                     y = pct)) +
+          geom_col(fill = mna.blue) +
+          geom_text(aes(label = percent(pct)),
+                    color = 'white',
+                    hjust = 1.25) +
+          scale_y_continuous(limits = c(0, 1),
+                             breaks = pretty_breaks(),
+                             labels = percent) +
+          coord_flip() +
+          mna.bar.chart.theme
+     
+}
+
+
+
+# Sentence case -----------------------------------------------------------
+
+# https://stackoverflow.com/questions/22976472/capitalize-the-first-word-of-a-sentence-regex-gsub-gregexpr
+
+dk_sentence_case <- function(x) {
+     
+     gsub("([^.!?\\s])([^.!?]*(?:[.!?](?!['\"]?\\s|$)[^.!?]*)*[.!?]?['\"]?)(?=\\s|$)", 
+          "\\U\\1\\E\\2", 
+          x, 
+          perl=T, 
+          useBytes = F)
+}
+
+
 # Make bulleted list ------------------------------------------------------
 
 dk_bulleted_list <- function(string.list) {
@@ -91,3 +169,30 @@ dk_bulleted_list <- function(string.list) {
      cat(paste('-', string.list), sep = '\n')
      
 }
+
+# Prep quotes -------------------------------------------------------------
+
+dk_prep_quotes <- function(x) {
+     x %>%
+          set_names('response') %>%
+          filter(!is.na(response)) %>%
+          filter(str_length(response) > 4) %>%
+          # filter(!str_detect(response, "Nothing")) %>%
+          arrange(-str_length(response)) %>%
+          pull(response) %>%
+          str_collapse_whitespace() %>%
+          dk_sentence_case() %>%
+          dk_bulleted_list()
+     
+}
+
+
+# Summarize N and pct -----------------------------------------------------
+
+dk_n_pct <- function(df, ...) {
+     janitor::tabyl(df, ...) %>%
+          janitor::adorn_pct_formatting(digits = 2) %>%
+          janitor::adorn_totals("row")
+}
+
+
